b4a9e6e5b38716c2a1cc4a86e7057288ab559446
CM Student <cmstudent@UHWO-ACM-C-01.local>
2018-03-05 10:49:32 -1000

    Finished hackwitch6

--- Hackwitch6/ViewController.swift
+++ Hackwitch6/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Slider: UISlider!
+    var currentValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func sliderHasMoved(_ sender: Any) {
+        print("The value of the slider is:\(Slider.value)")
+        currentValue = lroundf(Slider.value)
+    }
+    
+    @IBAction func MyGuessButtonPressed(_ sender: Any) {
+    
+        let message = " The value is: \(currentValue)"
+        let alert = UIAlertController(title:"Hello World!", message: message, preferredStyle: . alert)
+        let action = UIAlertAction(title: " Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    
+    }
+    
+    
+  
+    
+   
 }
 
